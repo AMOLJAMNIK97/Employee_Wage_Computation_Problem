88771cb30718533cd13be68e445848593c44c1e1
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-23 12:28:10 +0530

    Added: Add Part time Employee & Wage

--- forloop.sh
+++ forloop.sh
@@ -1,14 +1,17 @@
-isPresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ];
 then
-	echo "Employee is Present"
-	empRatePerHr=20;
-	empHr=8;
-	salary=$(( $empHr * $empRatePerHr ));
-	echo "Employee Salary:" $salary
+	empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+	empHrs=4;
 else
-	echo "Employee is Absent"
-salary=0:
+	empHrs=0;
 fi
+
+salary=$(($empHrs*$empRatePerHr))
+echo "Employee Salary:" $salary
