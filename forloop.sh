67c1f37b2cc8ee2c716e1745a16e3e385d7a503b
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-23 11:59:36 +0530

    Added: Calculate Daily Employee Wage

--- forloop.sh
+++ forloop.sh
@@ -4,6 +4,11 @@ randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ];
 then
 	echo "Employee is Present"
+	empRatePerHr=20;
+	empHr=8;
+	salary=$(( $empHr * $empRatePerHr ));
+	echo "Employee Salary:" $salary
 else
 	echo "Employee is Absent"
+salary=0:
 fi
